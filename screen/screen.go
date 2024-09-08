cafe6146d1bd7ad6f9134ebccb03b8d79e767101
Grygoriy Budilovsky <jumbleview@gmail.com>
2024-09-08 10:58:05 -0700

    Image file reading, croppping, and saving moved out of platform dependent file screen_windows.go

--- screen/screen.go
+++ screen/screen.go
@@ -1,3 +1,84 @@
 package screen
 
+import (
+	"bufio"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"os"
 
+	"golang.org/x/image/bmp"
+)
+
+// EncodeCropDecode reads jpeg file, crops omage to match monitor ratio, encode image as bmp file
+func EncodeCropDecode(jpegName, bmpName string, monitorWidth, monitorHeight int) error {
+
+	fconfig, err := os.Open(jpegName)
+
+	if err != nil {
+		return err
+	}
+	// Read picture format and pixel size
+	config, format, err := image.DecodeConfig(fconfig)
+	fconfig.Close()
+	if err != nil {
+		return err
+	}
+
+	pictureRatio := float64(config.Width) / float64(config.Height)
+	monitorRatio := float64(monitorWidth) / float64(monitorHeight)
+	cropWigth := config.Width
+	cropHeight := monitorHeight * cropWigth / monitorWidth
+	cropX := 0
+	cropY := (config.Height - cropHeight) / 2
+	if pictureRatio > monitorRatio {
+		cropHeight = config.Height
+		cropWigth = monitorWidth * cropHeight / monitorHeight
+		cropX = (config.Width - cropWigth) / 2
+		cropY = 0
+	}
+	fmt.Printf("Image  type: %s; size: %d*%d\n", format, config.Width, config.Height)
+	fmt.Printf("Monitor size: %d*%d\n", monitorWidth, monitorHeight)
+	fmt.Printf("Crop: %d, %d; %d %d\n", cropWigth, cropHeight, cropX, cropY)
+
+	f, err := os.Open(jpegName)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if format != "jpeg" {
+		return fmt.Errorf("image format is %s != jpeg", format)
+	}
+	// Decode jpeg file into image
+	jpegr := bufio.NewReader(f)
+	imgFull, err := jpeg.Decode(jpegr)
+	if err != nil {
+		return err
+	}
+
+	// Crop image to fit monitor screen to avoid image distortion
+	var imgCrop image.Image
+	type subImager interface {
+		SubImage(r image.Rectangle) image.Image
+	}
+	simg, ok := imgFull.(subImager)
+	rect := image.Rect(cropX, cropY, cropWigth, cropHeight)
+
+	if ok {
+		imgCrop = simg.SubImage(rect)
+	}
+
+	fbmp, err := os.Create(bmpName)
+	if err != nil {
+		return err
+	}
+	defer fbmp.Close()
+	// encode image as bmp file
+	bmpw := bufio.NewWriter(fbmp)
+	if ok {
+		err = bmp.Encode(bmpw, imgCrop)
+	} else {
+		err = bmp.Encode(bmpw, imgFull)
+	}
+	return err
+}
--- screen/screen_windows.go
+++ screen/screen_windows.go
@@ -1,16 +1,10 @@
 package screen
 
 import (
-	"bufio"
-	"fmt"
-	"image"
-	"image/jpeg"
 	"os"
 	"path/filepath"
 	"syscall"
 	"unsafe"
-
-	"golang.org/x/image/bmp"
 )
 
 // MakeConsole allocates console for the app built with -H=windowsgui flag
@@ -38,11 +32,12 @@ const (
 	SM_CYSCREEN         = 1
 )
 
-// SetWallpaper decodes jpeg file into bmp and sets it as windows background
+// SetWallpaper decodes jpeg file into bmp and after cropping sets it as windows background
 func SetWallpaper(jpegName, currentPath string) error {
 	user32 := syscall.NewLazyDLL("user32.dll")
 	proc := user32.NewProc("SystemParametersInfoW")
 	metrics := user32.NewProc("GetSystemMetrics")
+
 	// Read Monitor X, Y resolution
 	xNumber, _, _ := metrics.Call(SM_CXSCREEN)
 	yNumber, _, _ := metrics.Call(SM_CYSCREEN)
@@ -50,86 +45,23 @@ func SetWallpaper(jpegName, currentPath string) error {
 	monitorWidth := int(*(*int)(unsafe.Pointer(&xNumber)))
 	monitorHeight := int(*(*int)(unsafe.Pointer(&yNumber)))
 
-	fconfig, err := os.Open(jpegName)
-
-	if err != nil {
-		return err
-	}
-	// Read picture format and pixel size
-	config, format, err := image.DecodeConfig(fconfig)
-	fconfig.Close()
-	if err != nil {
-		return err
-	}
-
-	pictureShape := float64(config.Width) / float64(config.Height)
-	monitorShape := float64(monitorWidth) / float64(monitorHeight)
-	cropWigth := config.Width
-	cropHeight := monitorHeight * cropWigth / monitorWidth
-	cropX := 0
-	cropY := (config.Height - cropHeight) / 2
-	if pictureShape > monitorShape {
-		cropHeight = config.Height
-		cropWigth = monitorWidth * cropHeight / monitorHeight
-		cropX = (config.Width - cropWigth) / 2
-		cropY = 0
-	}
-	fmt.Printf("Image  type: %s; size: %d*%d\n", format, config.Width, config.Height)
-	fmt.Printf("Monitor size: %d*%d\n", monitorWidth, monitorHeight)
-	fmt.Printf("Crop: %d, %d; %d %d\n", cropWigth, cropHeight, cropX, cropY)
-
-	f, err := os.Open(jpegName)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	if format != "jpeg" {
-		return fmt.Errorf("image format is %s != jpeg", format)
-	}
-	// Decode jpeg file into image
-	jpegr := bufio.NewReader(f)
-	imgFull, err := jpeg.Decode(jpegr)
-	if err != nil {
-		return err
-	}
-
-	// Crop image to fit monitor screen to avoid image distortion
-	var imgCrop image.Image
-	type subImager interface {
-		SubImage(r image.Rectangle) image.Image
-	}
-	simg, ok := imgFull.(subImager)
-	rect := image.Rect(cropX, cropY, cropWigth, cropHeight)
-
-	if ok {
-		imgCrop = simg.SubImage(rect)
-	}
-
 	bmpName := filepath.Join(currentPath, "jumbleview_current.bmp")
-	fbmp, err := os.Create(bmpName)
+	// Convert JPG file into image, crop it, and save as BMP file
+	err := EncodeCropDecode(jpegName, bmpName, monitorWidth, monitorHeight)
 	if err != nil {
 		return err
 	}
-	defer fbmp.Close()
-	// encode image as bmp file
-	bmpw := bufio.NewWriter(fbmp)
-	if ok {
-		err = bmp.Encode(bmpw, imgCrop)
-	} else {
-		err = bmp.Encode(bmpw, imgFull)
-	}
-	if err != nil {
-		return err
-	}
-	fmt.Printf("%s\n", bmpName)
+
+	// Set BMP file as background
 	address, err := syscall.UTF16PtrFromString(bmpName)
-	if err == nil {
-		proc.Call(
-			spiSETDESKWALLPAPER,
-			0,
-			uintptr(unsafe.Pointer(address)),
-			spifUPDATEINIFILE,
-		)
+	if err != nil {
+		return err
 	}
+	_, _, err = proc.Call(
+		spiSETDESKWALLPAPER,
+		0,
+		uintptr(unsafe.Pointer(address)),
+		spifUPDATEINIFILE,
+	)
 	return err
 }
